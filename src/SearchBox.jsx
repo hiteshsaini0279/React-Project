9e54a3521fa55eabd80afe3bd92202ccc3311bcc
Hitesh saini <hiteshsaini0277@gmail.com>
2026-03-10 14:53:51 +0530

    api issue

--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -5,7 +5,7 @@ import { useState } from 'react';
 export default function SearchBox(){
      let [city, setCity]=useState("");
     const API_URL="https://api.openweathermap.org/data/2.5/weather";
-    const API_KEY="744e1db4dd089fd000449dbd807cd984";
+    const API_KEY= import.meta.env.API_KEY_WEATHER;
     let  getWeather=async()=>{
       let response=  await fetch(`${API_URL}?q=${city}&appid=${API_KEY}&units=metric`);
       let getResponse=await response.json();
